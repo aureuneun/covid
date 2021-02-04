5f3bde37feb52fa1ff1473ad079d80028012cace
aureuneun <aureuneun@gmail.com>
2021-02-04 12:47:43 +0900

    feat: interactivity by callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 from data import make_daily_df, make_time_df
 from builder import make_table
@@ -8,6 +9,9 @@ from builder import make_table
 df = make_daily_df()
 totals_df = df["totals_df"]
 countries_df = df["countries_df"]
+
+dropdown_options = countries_df.sort_values("Country_Region").reset_index()
+dropdown_options = dropdown_options["Country_Region"]
 
 country_df = make_time_df("Korea, South")
 global_df = make_time_df()
@@ -38,7 +42,9 @@ bubbles_map = px.scatter_geo(
     title="Confirmed By Country",
     template="plotly_dark",
 )
-bubbles_map.update_layout(margin=dict(r=0, l=0))
+bubbles_map.update_layout(
+    margin=dict(r=0, l=0, b=0), coloraxis_colorbar=dict(xanchor="left", x=0)
+)
 
 bars_graph = px.bar(
     totals_df,
@@ -52,12 +58,12 @@ bars_graph = px.bar(
     },
     template="plotly_dark",
     title="Total Global Cases",
-    labels={"color": "Conditions"},
+    labels={"color": "Condition"},
 )
 bars_graph.update_layout(
-    xaxis=dict(title="Conditions"),
+    xaxis=dict(title="Condition"),
     yaxis=dict(
-        title="Counts",
+        title="Count",
     ),
 )
 # bars_graph.update_traces(marker_color=["#e74c3c", "#8e44ad", "#27ae60"])
@@ -112,11 +118,53 @@ app.layout = html.Div(
                 html.Div(
                     children=[dcc.Graph(figure=bars_graph)],
                 ),
-                html.Div(),
+                html.Div(
+                    children=[
+                        dcc.Dropdown(
+                            id="input",
+                            style={
+                                "width": 320,
+                                "margin": "0 auto",
+                                "color": "#111111",
+                            },
+                            options=[
+                                {"label": option, "value": option}
+                                for option in dropdown_options
+                            ],
+                        ),
+                        dcc.Graph(id="output"),
+                    ]
+                ),
             ],
         ),
     ],
 )
+
+
+@app.callback(
+    Output("output", "figure"),
+    [
+        Input(
+            "input",
+            "value",
+        )
+    ],
+)
+def hello(value):
+    df = make_time_df(value)
+    figure = px.line(
+        df,
+        x="Date",
+        y=["Confirmed", "Deaths", "Recovered"],
+        template="plotly_dark",
+        hover_data={
+            "value": ":,",
+            "Date": False,
+            "variable": False,
+        },
+        labels={"value": "Cases", "variable": "Condition"},
+    )
+    return figure
 
 
 if __name__ == "__main__":
